7b9a0d1a26d1701bcb78abb54497f2664d9b8d17
KFad <kfadigan@yahoo.com>
2019-03-16 23:13:02 -0400

    Bunch of methods

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf)
+  dwarf.each_with_index.map {|val, index| puts "#{index + 1}. #{val}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planetcheers)
+  planetcheers.collect {|x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call)
+  if call.count >= 4
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+    cheese.detect do |i| cheese_types.include?(i)
+  end
 end
